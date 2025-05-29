1524b92d742c9700ef32e5b5fa08930c671cc612
🏳‍🌈 O9Creeps 🐯 <zslaba8949@inst.hcpss.org>
2025-05-29 13:25:09 +0000

    wawaawawwaa

--- text-anim.js
+++ text-anim.js
@@ -3,16 +3,22 @@ var hurr = document.getElementsByClassName('fl-Left');
 const div = document.getElementById("hurrdurr");
 let colhurr = document.getElementById("anim-hurr").style.color;
 let coldurr = document.getElementById("anim-durr").style.color;
+let state = 0;
 
 function textanim() {
-    if (colhurr == "rgb(240, 248, 255)") {
-        document.getElementById("anim-hurr").style.color = 'rgb(255, 80, 80)'
-        document.getElementById("anim-durr").style.color = 'rgb(240, 248, 255)'
-    }
-    else {
-        document.getElementById("anim-hurr").style.color = 'rgb(240, 248, 255)'
-        document.getElementById("anim-durr").style.color = 'rgb(235, 235, 0)'
+    if (state == 1) {
+        document.getElementById("anim-hurr").style.color = "rgb(255, 80, 80)";
+        document.getElementById("anim-durr").style.color = "rgb(240, 248, 255)";
+        state = 0;
+    } else {
+        document.getElementById("anim-hurr").style.color = "rgb(240, 248, 255)";
+        document.getElementById("anim-durr").style.color = "rgb(235, 235, 0)";
+        state = 1;
     }
 }
 
-setInterval(textanim,1000);
+for (var i = 0; i < 1; i++) {
+
+    setInterval(textanim,800);
+
+}
