8d549ea131131c3962ba4c2b4f2c81ffbe84ce01
🏳‍🌈 O9Creeps 🐯 <zslaba8949@inst.hcpss.org>
2025-05-29 14:46:16 +0000

    youtub

--- scroll.js
+++ scroll.js
@@ -1,3 +1,10 @@
-document.addEventListener('scroll', () => {
-    document.documentElement.dataset.scroll = window.scrollY;
-});
+document.getElementById("body").onscroll = function myFunction() {  
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("text-01");
+    var xvalue = "sticky";
+    var factor = 0.5;
+    var yvalue = scrolltotop * factor;
+    var wa = 20;
+    target.style.position = xvalue;
+    target.style.top = yvalue + "px";
+  }
